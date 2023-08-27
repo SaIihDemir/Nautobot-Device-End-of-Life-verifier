4e7815432e9b5a881445e02623ee195efa43bae1
Salih Demir <Salihdemir2015@outlook.de>
2023-08-27 22:51:16 +0200

    init

---
+++ jobs/EOL-Examiner.py
@@ -0,0 +1,36 @@
+
+from nautobot.dcim.models import Device
+from nautobot.extras.jobs import Job
+from datetime import datetime, date
+
+ 
+class VerifyEOL(Job) :
+ class Meta:
+         description = """
+             this job does the following                           
+             
+             1.Lists every device with a expired EOL(End-of-Life).
+             2.Shows the contact person, whos EOL has expired.
+             """
+         name = "Function to compare eol dates with today and filter obsolete devices."
+ 
+ def run (self, data, commit):
+     device = Devices.objects.all()  
+     eol_date_str = Devices.custom_fields.get('eol', None)
+ 
+     obsolete_devices = []                                           #create a list for expired devices
+         
+     for device in Devices.objects.all():
+             eol_date_str = Device.custom_fields.get('eol', None)  
+             if eol_date_str:
+                 eol_date = datetime.strptime(eol_date_str, '%Y-%m-%d').date()
+                 if eol_date < date.today():
+                     obsolete_devices.append(display)
+         
+ 
+     if obsolete_devices:
+                 self.log_failure("Check the following obsolete devices:")
+                 for device in obsolete_devices:
+                     self.log_failure(device)
+ 
+ jobs = (VerifyEOL)
---
+++ jobs/__init__.py
@@ -0,0 +1,2 @@
+# Datei: __init__.py
+
