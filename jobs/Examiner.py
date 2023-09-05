f2d56f4bb5567a8dbfd4c0302fe995b819cfbd1d
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-05 11:42:21 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -19,11 +19,10 @@ class VerifyEOL(Job) :
                eol=datetime.strptime(eol, '%Y-%m-%d').date()
                if eol < date.today():
                   obsolete_devices.append(device)
-                  sorted_obsolete_devices = sorted(obsolete_devices(key= 0,  reverse = False))
                   for devices in sorted_obsolete_devices:
                       contact = device.cf["contact"], devices, devices.cf["eol"]
-                      self.log_failure(obj= sorted_obsolete_devices, message = "Inform GWDG and the contact about this Device")
-  return contact
+                      self.log_failure(obj= obsolete_devices, message = "Inform GWDG and the contact about this Device")
+  return obsolete_devices
                      
                   
                                    
