c3f9c32cb29c3368f5d1dbd396bee06cd882f766
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-05 10:47:54 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -23,7 +23,7 @@ class VerifyEOL(Job) :
                  
                     for devices in obsolete_devices:
                         contact = device.cf["contact"], devices, devices.cf["eol"]
-                        s_contact = sorted(contact)
+                        s_contact = sorted(contact.cf["contact"])
                         self.log_failure(obj= s_contact, message = "Inform GWDG and the contact about this Device")
   return obsolete_devices
                      
