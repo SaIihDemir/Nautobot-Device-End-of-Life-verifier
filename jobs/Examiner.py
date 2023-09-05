39c78aef5ea2a8827c19166ac9ae5a4597f629d8
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-05 09:53:19 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -12,17 +12,17 @@ class Verifyer(Job) :
          name = "Function : compare eol date with today and filter obsolete devices."
   
 def run(self, data, commit):  
-        obsolete_devices = []
+    obsolete_devices = []
  
-        for device in Device.objects.all():
-            self.log_success(obj = device, message = "YOO")
-            eol = device.cf["eol"]         
-            eol = datetime.strptime(eol, '%Y-%m-%d').date()
-            if eol < date.today():
-               obsolete_devices.append(device)
+    for device in Device.objects.all():
+        self.log_success(obj = device, message = "YOO")
+        eol = device.cf["eol"]         
+        eol = datetime.strptime(eol, '%Y-%m-%d').date()
+        if eol < date.today():
+           obsolete_devices.append(device)
 
-            for devices in obsolete_devices:
-                self.log_failure(obj = devices, message = "test")
+        for devices in obsolete_devices:
+            self.log_failure(obj = devices, message = "test")
                  
                                    
                                    
