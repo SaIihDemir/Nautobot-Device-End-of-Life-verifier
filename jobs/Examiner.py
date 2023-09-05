4693b264fdb11ecd9c6b069f97b7e65d1e5d692a
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-05 09:56:44 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -1,30 +1,34 @@
 from nautobot.dcim.models import Device
 from nautobot.extras.jobs import Job
-from datetime import datetime, date, timedelta
+from datetime import datetime, date
+from itertools import groupby
 
-
-class Verifyer(Job) :
+class VerifyEOL(Job) :
  class Meta:
          description = """
-             1.Lists every device with a expired EOL(End-of-Life).
-             2.Shows the contact person, whos EOL has expired.
+             1.Lists every device with an expired EOL(End-of-Life).
+             2.Shows who the device belongs to, including the contact information .
              """
-         name = "Function : compare eol date with today and filter obsolete devices."
-  
-def run(self, data, commit):  
-    obsolete_devices = []
- 
-    for device in Device.objects.all():
-        self.log_success(obj = device, message = "YOO")
-        eol = device.cf["eol"]         
-        eol = datetime.strptime(eol, '%Y-%m-%d').date()
-        if eol < date.today():
-           obsolete_devices.append(device)
-
-        for devices in obsolete_devices:
-            self.log_failure(obj = devices, message = "test")
+         name = "Compare EOL date with today and filter obsolete devices."
+         
+        
+ def run (self, data, commit):
+  self.log_info(obj=None, message = "Examine the following Devices: ")
+  for device in Device.objects.all():
+            eol = device.cf["eol"]
+            obsolete_devices = []
+            if eol:
+                eol=datetime.strptime(eol, '%Y-%m-%d').date()
+                if eol < date.today():
+                    obsolete_devices.append(device)
                  
-                                   
+                   # for devices in obsolete_devices:
+                       # eol = devices[1]["contact"], devices[0]
+                       # self.log_failure(obj=eol, message = "Inform GWDG and the contact about this Device")
+                     
+                   # contact_dict = {}
+                    for line in eol:             
+                        self.log_success(obj= line, message = "YO")
                                    
                                    
                                    
