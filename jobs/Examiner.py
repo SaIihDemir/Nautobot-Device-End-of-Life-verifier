74d94f1a9629c3a2dda0a8393d802f13143e9e2f
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-06 09:05:18 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -31,7 +31,7 @@ class VerifyEOL(Job) :
              
                #writer.writerow(field)
             #sorted_obsolete_devices=sorted(obsolete_devices,key=itemgetter(0))
-  for x in obsolete_devices
+  for x in obsolete_devices:
       ''.join(x)
   return x
                      
