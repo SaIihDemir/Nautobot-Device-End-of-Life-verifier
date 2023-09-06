f265a9cadea8f92743d2b779911c47d2837833ec
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-06 11:30:17 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -22,32 +22,64 @@ class VerifyEOL(Job) :
             eol=datetime.strptime(eol, '%Y-%m-%d').date()
             if eol < date.today():
                obsolete_devices.append([device.cf["contact"],device.name, device.cf["eol"]]) 
+         from nautobot.dcim.models import Device
+from nautobot.extras.jobs import Job
+from datetime import datetime, date
+import csv
+from operator import itemgetter
+
+class VerifyEOL(Job) :
+ class Meta:
+         description = """
+             1.Lists every device with an expired EOL(End-of-Life).
+             2.Shows who the device belongs to, including the contact information .
+             """
+         name = "Compare EOL date with today and filter obsolete devices."
          
+        
+ def run (self, data, commit):
+  
+      for device in Device.objects.all():
+         if device: 
+            eol = device.cf["eol"]
+            obsolete_devices = []
+            eol=datetime.strptime(eol, '%Y-%m-%d').date()
+            if eol < date.today():
+               obsolete_devices.append([device.cf["contact"],device.name, device.cf["eol"]]) 
+      
       if obsolete_devices:
-         obsolete_devices = sorted(obsolete_devices, key=itemgetter(0))   
-         with open('obsolete_devices.csv', 'w', newline='') as file:
+         sorted_obsolete_devices=sorted(obsolete_devices,key=itemgetter(0))       
+      else:
+            self.log_failure(obj=None, message = "no obsolete Device found")   
+         
+         
+      with open('obsolete_devices.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             field = ['Contact', 'Device', 'EOL']
             writer.writerow(field) 
             for devices in obsolete_devices:
                 writer.writerow(devices)
-         self.log_success(obj=None , message = "created list with obsolete Devices")     
+            self.log_success(obj=None , message = "created list with obsolete Devices")  
+      if obsolete_devices:
+         sorted_obsolete_devices=sorted(obsolete_devices,key=itemgetter(0))       
       else:
             self.log_failure(obj=None, message = "no obsolete Device found")
            
-                     
-                     
+         
+      if obsolete_devices:
+         sorted_obsolete_devices=sorted(obsolete_devices,key=itemgetter(0))   
+      with open('obsolete_devices.csv', 'w', newline='') as file:
+          writer = csv.writer(file)
+          field = ['contact', 'Device', 'EOL']              
+          writer.writerow(field)
+      self.log_success(obj=None , message = "created list with obsolete Devices") 
+      for device in obsolete_devices:
+            writer.writerow(device)
+      self.log_success(obj=None , message = "created list with obsolete Devices") 
+
+           
                      
                      
                   
                                    
                   
-                     
-                                   
-                                   
-                                   
-                                   
-                                   
-                                   
-                           
- 
