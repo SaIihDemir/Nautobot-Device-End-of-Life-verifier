56038be3d8eeea287969bcdcb3e220c2b38c93c6
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-22 14:08:46 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -54,10 +54,10 @@ class VerifyEOL(Job) :
           
       for one_contact in contact_devices:
           if len(one_contact) >2:
-              self.log_warning(obj=contact, message = "Kontakt")
+              self.log_success(obj=contact, message = "Kontakt")
               for c in (one_contact[0:len(one_contact)-1]):
                   only_one_contact.append([c,one_contact[-1]])
-                  self.log_warning(obj=c, message = "C")
+                  self.log_failure(obj=c, message = "C")
           else:
               only_one_contact.append(one_contact)
       only_one_contact = sorted(only_one_contact, key=itemgetter(0))   
