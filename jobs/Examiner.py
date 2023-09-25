90d48c24f624a2e1b17c2da9ce4af6cabf72428f
SaIihDemir <94231284+SaIihDemir@users.noreply.github.com>
2023-09-25 14:23:19 +0200

    Update Examiner.py

--- jobs/Examiner.py
+++ jobs/Examiner.py
@@ -57,7 +57,7 @@ class VerifyEOL(Job) :
 #split multiple mail adresses string seperate strings with devices
       split_contacts = []                  
       for contact_with_device in contact_devices:
-          seperated_mail = re.split(r"[,]\s*", contact_with_device.cf["contact"])
+          seperated_mail = re.split(r"[,]\s*", contact_with_device[0])
           split_contacts.append([seperated_mail,contact_with_device[-1]])
             
       one_mail_with_devices = []      
